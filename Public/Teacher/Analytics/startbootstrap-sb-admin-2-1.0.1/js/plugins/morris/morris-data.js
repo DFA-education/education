2d2ec8ca59decf450730582efb29510656835aa1
Caroline Chin <cmchin@mit.edu>
2014-12-09 17:11:47 -0500

    making analytics thing. gave up using all morris

--- Public/Teacher/Analytics/startbootstrap-sb-admin-2-1.0.1/js/plugins/morris/morris-data.js
+++ Public/Teacher/Analytics/startbootstrap-sb-admin-2-1.0.1/js/plugins/morris/morris-data.js
@@ -3,59 +3,50 @@ $(function() {
     Morris.Area({
         element: 'morris-area-chart',
         data: [{
-            period: '2010 Q1',
-            iphone: 2666,
-            ipad: null,
-            itouch: 2647
+            period: '2013-08',
+            ConceptQ: 5,
+            ShortAnswer: 40,
         }, {
-            period: '2010 Q2',
-            iphone: 2778,
-            ipad: 2294,
-            itouch: 2441
+            period: '2013-09',
+            ConceptQ: 15,
+            ShortAnswer: 45,
         }, {
-            period: '2010 Q3',
-            iphone: 4912,
-            ipad: 1969,
-            itouch: 2501
+            period: '2013-10',
+            ConceptQ: 20,
+            ShortAnswer: 50,
         }, {
-            period: '2010 Q4',
-            iphone: 3767,
-            ipad: 3597,
-            itouch: 5689
+            period: '2013-11',
+            ConceptQ: 22,
+            ShortAnswer: 55,
         }, {
-            period: '2011 Q1',
-            iphone: 6810,
-            ipad: 1914,
-            itouch: 2293
+            period: '2013-12',
+            ConceptQ: 20,
+            ShortAnswer: 53,
         }, {
-            period: '2011 Q2',
-            iphone: 5670,
-            ipad: 4293,
-            itouch: 1881
+            period: '2014-01',
+            ConceptQ: 24,
+            ShortAnswer: 50,
         }, {
-            period: '2011 Q3',
-            iphone: 4820,
-            ipad: 3795,
-            itouch: 1588
+            period: '2014-02',
+            ConceptQ: 21,
+            ShortAnswer: 60,
         }, {
-            period: '2011 Q4',
-            iphone: 15073,
-            ipad: 5967,
-            itouch: 5175
+            period: '2014-03',
+            ConceptQ: 23,
+            ShortAnswer: 66,
         }, {
-            period: '2012 Q1',
-            iphone: 10687,
-            ipad: 4460,
-            itouch: 2028
+            period: '2014-04',
+            ConceptQ: 21,
+            ShortAnswer: 70,
         }, {
-            period: '2012 Q2',
-            iphone: 8432,
-            ipad: 5713,
-            itouch: 1791
+            period: '2014-05',
+            ConceptQ: 25,
+            ShortAnswer: 75,
         }],
         xkey: 'period',
-        ykeys: ['iphone', 'ipad', 'itouch'],
-        labels: ['iPhone', 'iPad', 'iPod Touch'],
+        xLabels: 'month',
+        ykeys: ['ConceptQ', 'ShortAnswer'],
+        labels: ['ConceptQ', 'ShortAnswer'],
         pointSize: 2,
         hideHover: 'auto',
         resize: true
@@ -64,13 +55,13 @@ $(function() {
     Morris.Donut({
         element: 'morris-donut-chart',
         data: [{
-            label: "Download Sales",
-            value: 12
+            label: "Astronaut",
+            value: 40
         }, {
-            label: "In-Store Sales",
+            label: "Doctor",
             value: 30
         }, {
-            label: "Mail-Order Sales",
+            label: "Engineer",
             value: 20
         }],
         resize: true
@@ -79,36 +70,36 @@ $(function() {
     Morris.Bar({
         element: 'morris-bar-chart',
         data: [{
-            y: '2006',
-            a: 100,
-            b: 90
+            y: 'August',
+            classA: 50,
+            classB: 60
         }, {
-            y: '2007',
-            a: 75,
-            b: 65
+            y: 'September',
+            classA: 55,
+            classB: 68
         }, {
-            y: '2008',
-            a: 50,
-            b: 40
+            y: 'October',
+            classA: 69,
+            classB: 65
         }, {
-            y: '2009',
-            a: 75,
-            b: 65
+            y: 'November',
+            classA: 75,
+            classB: 71
         }, {
-            y: '2010',
-            a: 50,
-            b: 40
+            y: 'December',
+            classA: 78,
+            classB: 75
         }, {
-            y: '2011',
-            a: 75,
-            b: 65
+            y: 'January',
+            classA: 81,
+            classB: 85
         }, {
-            y: '2012',
-            a: 100,
-            b: 90
+            y: 'Febuary',
+            classA: 100,
+            classB: 100
         }],
         xkey: 'y',
-        ykeys: ['a', 'b'],
+        ykeys: ['classA', 'classB'],
         labels: ['Series A', 'Series B'],
         hideHover: 'auto',
         resize: true
